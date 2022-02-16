7d852ccce2aa543323638fcff4299121bd125bd9
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-02-16 18:34:24 +0100

    fixed some details for heroku

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 app.use(formidable());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.all("*", (req, res) => {
   res.json({ message: "Shu lo lo lo lo" });
 });
 
-// api key L2bUh2Wdgv0GF7mV
-
-app.listen(3000, (req, res) => {
-  console.log("Server Started !!!");
+app.listen(process.env.PORT || 3200, () => {
+  console.log("Server started");
 });
