42fa18422a2fdcdbbfae6e7462c7a8aebe9a26d7
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-02-20 04:59:55 +0100

    setting cors

--- app.js
+++ app.js
@@ -7,12 +7,39 @@ const app = express();
 app.use(cors());
 app.use(formidable());
 
+const http = require("http");
+const httpServer = http.createServer();
+
+httpServer.on("request", (request, response) => {
+  // On spécifie l'entête pour le CORS
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  // On gère le cas où le navigateur fait un pré-contrôle avec OPTIONS ...
+  // ... pas besoin d'aller plus loin dans le traitement, on renvoie la réponse
+  if (request.method === "OPTIONS") {
+    // On liste des méthodes et les entêtes valides
+    response.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Accept, Origin, Authorization"
+    );
+    response.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+
+    return response.end();
+  }
+
+  // suite du traitement ...
+});
+
 mongoose.connect(process.env.MONGODB_URI);
 
 const user = require("./user");
 app.use(user);
 
 const userFavorites = require("./Routes/userFavorites");
+app.use(userFavorites);
 
 const comicsList = require("./Routes/comicslist");
 app.use(comicsList);
@@ -30,6 +57,8 @@ app.all("*", (req, res) => {
   res.json({ message: "Nothing here" });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
+
+httpServer.listen(process.env.PORT);
